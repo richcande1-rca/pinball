1d10ef974012ce5489082ff56ef10cb50c033871
richcande1-rca <richcande1@gmail.com>
2026-08-30 22:44:04 -0500

    Bust cache for blended car decals

--- intro.js
+++ intro.js
@@ -75,7 +75,7 @@ window.addEventListener('load', () => {
     businessesScript.async = false;
     businessesScript.addEventListener('load', () => {
       const carsScript = document.createElement('script');
-      carsScript.src = 'cars.js?v=20260831-3qpolish';
+      carsScript.src = 'cars.js?v=20260831-carblend';
       carsScript.async = false;
       document.body.appendChild(carsScript);
     }, { once: true });
