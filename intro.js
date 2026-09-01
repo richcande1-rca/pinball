78337480b5bc3d463f38c9a37d7e71080fd5dfa3
richcande1-rca <richcande1@gmail.com>
2026-08-31 23:28:58 -0500

    Bust car feature cache for front-facing pair

--- intro.js
+++ intro.js
@@ -75,7 +75,7 @@ window.addEventListener('load', () => {
     businessesScript.async = false;
     businessesScript.addEventListener('load', () => {
       const carsScript = document.createElement('script');
-      carsScript.src = 'cars.js?v=20260831-carclip';
+      carsScript.src = 'cars.js?v=20260831-frontpair';
       carsScript.async = false;
       document.body.appendChild(carsScript);
     }, { once: true });
