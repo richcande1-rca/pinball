9713c9131507cbc026d9d937e0426a5791bba6d7
richcande1-rca <richcande1@gmail.com>
2026-09-01 00:26:46 -0500

    Refresh transparent car cache

--- intro.js
+++ intro.js
@@ -75,7 +75,7 @@ window.addEventListener('load', () => {
     businessesScript.async = false;
     businessesScript.addEventListener('load', () => {
       const carsScript = document.createElement('script');
-      carsScript.src = 'cars.js?v=20260831-ferrari3';
+      carsScript.src = 'cars.js?v=20260901-caralpha4';
       carsScript.async = false;
       document.body.appendChild(carsScript);
     }, { once: true });
