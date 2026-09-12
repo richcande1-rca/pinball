a5833374b20d700a1680708ae5456601ef499c3a
richcande1-rca <richcande1@gmail.com>
2026-09-12 04:10:42 -0500

    Bump release token for recovery mouth widening

--- intro.js
+++ intro.js
@@ -7,10 +7,10 @@
 (() => {
   if (window.miamiVersionedAsset) return;
 
-  const buildToken = '20260912-gate1b-recovery1b';
+  const buildToken = '20260912-gate1b-recovery1c';
   window.miamiBuildToken = buildToken;
   window.miamiCurrentBuildLabel =
-    'Build 20260912-PERF1-MB2-UP2A-LOWERBASE1-GATE1B-RECOVERY1B';
+    'Build 20260912-PERF1-MB2-UP2A-LOWERBASE1-GATE1B-RECOVERY1C';
 
   window.miamiVersionedAsset = function miamiVersionedAsset(path) {
     try {
